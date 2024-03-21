aab3b80f1477899951a120fbed437bc0e2f4a170
musthafa <musthafa.cp@kalvium.community>
2024-03-21 15:00:25 +0530

    news page updated

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,6 @@ const port = 3000;
 const connectDB = require("./config/connect")
 const pingrouter = require("./Routes/ping");
 const userrouter = require("./Routes/user");
-const latestnews = require("./Routes/news")
 
 const cors = require('cors');
 app.use(cors());
@@ -14,7 +13,6 @@ connectDB()
 app.use(express.json());
 app.use("/ping", pingrouter);
 app.use("/users", userrouter);
-app.use("/latestnews", latestnews);
 
 
 app.listen(port, () => {
