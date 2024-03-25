a8e9f0dbca51c9190137d94fdd265f15c69dd37a
musthafa <musthafa.cp@kalvium.community>
2024-03-25 16:25:56 +0530

    About page created

--- ClientCosmos/src/components/Landingpage.jsx
+++ ClientCosmos/src/components/Landingpage.jsx
@@ -24,14 +24,6 @@ function Landingpage() {
           Lets Explore
         </button>
       </motion.div>
-
-      <motion.div
-        className="slide-out"
-        initial={{ scaleY: 1 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleY: 0 }}
-        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-      />
     </>
   );
 }
