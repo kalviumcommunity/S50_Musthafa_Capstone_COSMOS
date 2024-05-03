93dfbe8493ec8d919cbb62660db6d44376433410
musthafa <musthafa.cp@kalvium.community>
2024-05-03 10:16:06 +0530

    added google auth for cap submission

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ const cors = require("cors");
 const passport = require("passport");
 const session = require("express-session");
 connectDB();
-
+// iii
 app.use(session({
   secret: SESSION_SECRET,
   resave: false,
