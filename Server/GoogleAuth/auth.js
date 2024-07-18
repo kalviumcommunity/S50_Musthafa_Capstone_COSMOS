f4840b2b55a8c9c3c2ae61b57f135e59367fc849
Musthafacp <141802711+Musthafacp@users.noreply.github.com>
2024-07-18 10:58:33 +0530

    Update auth.js

--- Server/GoogleAuth/auth.js
+++ Server/GoogleAuth/auth.js
@@ -25,6 +25,8 @@ router.get("/logout", (req, res) => {
       res.status(200).send("User logged out successfully");
     });
   });
+  res.clearCookie('token');
+  res.clearCookie('passwordisthere');
 });
 
 
