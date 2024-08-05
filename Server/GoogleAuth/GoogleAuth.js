bb9e314a3044fd92acff136a047a8be714d00045
Musthafacp <141802711+Musthafacp@users.noreply.github.com>
2024-08-05 18:15:00 +0530

    Update GoogleAuth.js

--- Server/GoogleAuth/GoogleAuth.js
+++ Server/GoogleAuth/GoogleAuth.js
@@ -17,91 +17,6 @@ const generateToken = (data) => {
   return token;
 };
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: GOOGLE_CLIENT_ID,
-//       clientSecret: GOOGLE_CLIENT_SECRET,
-//       // callbackURL: "http://localhost:3000/auth/google/callback",
-//       callbackURL: "https://s50-musthafa-capstone-cosmos.onrender.com/auth/google/callback",
-//       passReqToCallback: true,
-//     },
-//     async function (request, accessToken, refreshToken, profile, done) {
-//       try {
-//         async function createUser(profile) {
-//           return {
-//             name: profile.name.givenName,
-//             email: profile.email,
-//             password: "",
-//           };
-//         }
-
-//         const existingProfile = await Profilemodel.findOne({
-//           email: profile.email,
-//           name: profile.name.givenName,
-//         });
-
-//         if (existingProfile) {
-//           const existingUser = await usermodel.findOne({
-//             email: profile.email,
-//             name: profile.name.givenName,
-//           });
-
-//           if (!existingUser) {
-//             return done(new Error("User not found."));
-//           }
-
-//           const token = generateToken(existingProfile);
-
-//           request.res.cookie("token", token, {
-//             maxAge: 7 * 24 * 60 * 60 * 1000,
-//             httpOnly: false,
-//           });
-
-//           const passwordBool = false;
-//           request.res.cookie("passwordisthere", passwordBool, {
-//             maxAge: 7 * 24 * 60 * 60 * 1000,
-//             httpOnly: false,
-//           });
-
-//           return done(null, existingProfile);
-//         }
-
-//         const userDetail = await createUser(profile);
-//         const userData = await usermodel.create(userDetail);
-
-//         const userProfile = {
-//           name: profile.name.givenName,
-//           email: profile.email,
-//           posts: [],
-//           bio: "",
-//           communities: [],
-//           profilePic:
-//             "https://firebasestorage.googleapis.com/v0/b/cosmos-16de1.appspot.com/o/dp%2FScreenshot%202024-06-27%20011730.png?alt=media&token=46546fec-441e-4fe6-835a-5a39bda65c8a",
-//           user_id: userData._id,
-//         };
-//         const profileData = await Profilemodel.create(userProfile);
-
-//         const token = generateToken(profileData);
-
-//         request.res.cookie("token", token, {
-//           maxAge: 7 * 24 * 60 * 60 * 1000,
-//           httpOnly: false,
-//         });
-        // const passwordBool = false;
-//         request.res.cookie("passwordisthere", passwordBool, {
-//           maxAge: 7 * 24 * 60 * 60 * 1000,
-//           httpOnly: false,
-//         });
-
-//         return done(null, profileData);
-//       } catch (error) {
-//         return done(error);
-//       }
-//     }
-//   )
-// );
-
 passport.use(
   new GoogleStrategy(
     {
@@ -137,10 +52,6 @@ passport.use(
 
           const token = generateToken(existingProfile);
 
-          // req.session.token = token; 
-          // const passwordBool = false;
-          // req.session.passwordisthere = passwordBool;  
-
           return done(null, existingProfile);
         }
 
@@ -158,12 +69,7 @@ passport.use(
           user_id: userData._id,
         };
         const profileData = await Profilemodel.create(userProfile);
-
         const token = generateToken(profileData);
-
-        // req.session.token = token; 
-        // const passwordBool = false;
-        // req.session.passwordisthere = passwordBool;
 
         return done(null, profileData);
       } catch (error) {
