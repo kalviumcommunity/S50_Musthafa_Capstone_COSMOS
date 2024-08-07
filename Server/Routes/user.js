b66e3168684b4c7f30933a5c930fc6834850ed16
Musthafacp <141802711+Musthafacp@users.noreply.github.com>
2024-08-07 23:25:16 +0530

    Update user.js

--- Server/Routes/user.js
+++ Server/Routes/user.js
@@ -34,6 +34,7 @@ const generateToken = (data) => {
 const verifyToken = (req, res, next) => {
   const token =
     req.cookies.token || req.headers["x-access-token"] || req.body.token;
+   console.log(token)
 
   if (!token) {
     return res.status(200).json({ error: "Token is not provided" });
