2c5835b5293dd3f82eb00a642c89f0818f3c7f6c
musthafa <musthafa.cp@kalvium.community>
2024-08-24 17:06:39 +0530

    updated

--- Server/GoogleAuth/auth.js
+++ Server/GoogleAuth/auth.js
@@ -6,12 +6,38 @@ router.get('/google',
   passport.authenticate('google', { scope: ['email', 'profile'] })
 );
 
-router.get('/google/callback',
-  passport.authenticate('google', {
-    successRedirect: 'http://localhost:5173/HomePage',
-    failureRedirect: 'http://localhost:5173/login'
-  })
+// router.get('/google/callback',
+//   passport.authenticate('google', {
+//     successRedirect: 'http://localhost:5173/HomePage',
+//     failureRedirect: 'http://localhost:5173/login'
+//   })
+// );
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "http://localhost:5173/login" }),
+  (req, res) => {
+    console.log(req);
+    const { token, profile } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    const passwordBool = false;
+    res.cookie("passwordisthere", passwordBool, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.redirect("http://localhost:5173/HomePage");
+  }
 );
+
 
 router.get("/logout", (req, res) => {
   res.clearCookie("token", {
