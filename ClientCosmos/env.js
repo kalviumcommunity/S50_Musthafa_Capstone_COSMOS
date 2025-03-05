ea92a5e0415e2dcd1c33029fbc5088b7179bf76e
musthafa <musthafa.cp@kalvium.community>
2025-03-05 11:23:39 +0530

    Last changes

---
+++ ClientCosmos/env.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6T9JhGrAlgGpJbwNE5Ahyvxwoma8xLRI",
+  authDomain: "cosmos-16de1.firebaseapp.com",
+  projectId: "cosmos-16de1",
+  storageBucket: "cosmos-16de1.appspot.com",
+  messagingSenderId: "605832186824",
+  appId: "1:605832186824:web:d5942c1aed0de2d9d48efd",
+  measurementId: "G-SBRKJGGCZR",
+};
+
+export default firebaseConfig;
